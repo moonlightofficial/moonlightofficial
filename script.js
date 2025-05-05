284a5db5de0fb6c0634a9bec013e732273899abf
moonlightofficial <moonlightofficial63@gmail.com>
2025-05-06 03:16:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const button = document.getElementById('mainButton');
-const popup = document.getElementById('popup');
-const closeBtn = document.getElementById('closePopup');
+document.addEventListener("DOMContentLoaded", function () {
+    const mainButton = document.getElementById("mainButton");
+    const popup = document.getElementById("popup");
+    
+    mainButton.addEventListener("click", () => {
 
-button.addEventListener('click', () => {
-  popup.style.display = 'flex';
-  popup.classList.add('active');
-  setTimeout(() => {
-      window.location.href = 'https://discord.gg/4nyPNqhz8b';
-  }, 3000);
-});
+      popup.classList.add("show");
+  
+
+      setTimeout(() => {
+
+        document.querySelector('.popup-content h2').textContent = 'ยินดีต้อนรับเข้าสู่ Moonlight Project';
+        document.querySelector('.popup-content p').textContent = 'คุณเข้าสู่ระบบสำเร็จแล้ว!';
+  
+        setTimeout(() => {
+          window.open("https://discord.gg/4nyPNqhz8b", "_blank");
+        }, 1000);
+      }, 2000);
+    });
+  
+
+    document.getElementById("closePopup").addEventListener("click", () => {
+      popup.classList.remove("show");
+    });
+  });
+  
