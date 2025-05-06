63c398c550f99e21ea7f896529c2bd14b80cbfe1
moonlightofficial <moonlightofficial63@gmail.com>
2025-05-06 15:59:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,62 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
 
-    document.getElementById("closePopup").addEventListener("click", () => {
-      popup.classList.remove("show");
+    const closePopup = document.getElementById("closePopup");
+    if (closePopup) {
+      closePopup.addEventListener("click", () => {
+        popup.classList.remove("show");
+      });
+    }
+  });
+  
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const modeToggle = document.getElementById("modeToggle");
+    
+    if (localStorage.getItem('theme') === 'light') {
+      document.body.classList.add('light-mode');
+    } else {
+      document.body.classList.remove('light-mode');
+    }
+    
+    modeToggle.addEventListener("click", () => {
+
+      document.body.classList.toggle('light-mode');
+      
+      if (document.body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+      } else {
+        localStorage.setItem('theme', 'dark');
+      }
     });
+  });
+  
+  
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const starsContainer = document.querySelector('.stars-container');
+    const starCount = 100;
+  
+    for (let i = 0; i < starCount; i++) {
+      const star = document.createElement('div');
+      star.classList.add('star');
+  
+      star.style.top = Math.random() * 100 + 'vh';
+      star.style.left = Math.random() * 100 + 'vw';
+  
+      const size = Math.random() * 1.5 + 0.5;
+      star.style.width = size + 'px';
+      star.style.height = size + 'px';
+  
+      const duration = Math.random() * 4 + 2; // 2 - 6s
+      star.style.animationDuration = duration + 's';
+  
+      const delay = Math.random() * 5;
+      star.style.animationDelay = delay + 's';
+  
+      starsContainer.appendChild(star);
+    }
+  
   });
   
